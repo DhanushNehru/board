7c0057f9e0b99bb86f0492ad7e9bf2bafefe85c9
Dhanush Nehru <dhanushn@workhall.com>
2022-12-22 21:11:33 +0530

    formatting

--- colorChanges.js
+++ colorChanges.js
@@ -2,14 +2,13 @@ const canvas = document.getElementById("black-board");
 const boardColorInp = document.getElementById("board-color");
 const penColorInp = document.getElementById("pen-color");
 
-boardColorInp.addEventListener('change', (e)=>{
-    const color = e.target.value;
-    canvas.style.backgroundColor = color;
+boardColorInp.addEventListener("change", (e) => {
+  const color = e.target.value;
+  canvas.style.backgroundColor = color;
 });
 
-
-penColorInp.addEventListener('change', (e)=>{
-    const color = e.target.value;
-    const context = canvas.getContext("2d");
-    context.strokeStyle = color;
+penColorInp.addEventListener("change", (e) => {
+  const color = e.target.value;
+  const context = canvas.getContext("2d");
+  context.strokeStyle = color;
 });
