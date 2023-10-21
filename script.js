6bead4ede2174fed0166cf5ff56fccd59df6cb00
jeevaramanthan.m <jeevaramanathan.m@infosys.com>
2023-10-21 22:48:35 +0530

    google signin - initial commit

--- script.js
+++ script.js
@@ -335,6 +335,58 @@ window.addEventListener("beforeunload", function () {
   localStorage.setItem("oldImage", canvas.toDataURL());
 });
 
+// function handleCredentialResponse(response){
+//   // Post JWT token to server-side
+//   fetch("auth_init.php", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ request_type:'user_auth', credential: response.credential }),
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//       if(data.status == 1){
+//           let responsePayload = data.pdata;
+
+//           // Display the user account data
+//           let profileHTML = '<h3>Welcome '+responsePayload.given_name+'! <a href="javascript:void(0);" onclick="signOut('+responsePayload.sub+');">Sign out</a></h3>';
+//           profileHTML += '<img src="'+responsePayload.picture+'"/><p><b>Auth ID: </b>'+responsePayload.sub+'</p><p><b>Name: </b>'+responsePayload.name+'</p><p><b>Email: </b>'+responsePayload.email+'</p>';
+//           document.getElementsByClassName("pro-data")[0].innerHTML = profileHTML;
+          
+//           document.querySelector("#btnWrap").classList.add("hidden");
+//           document.querySelector(".pro-data").classList.remove("hidden");
+//       }
+//   })
+//   .catch(console.error);
+// }
+
+// // Sign out the user
+// function signOut(authID) {
+//   document.getElementsByClassName("pro-data")[0].innerHTML = '';
+//   document.querySelector("#btnWrap").classList.remove("hidden");
+//   document.querySelector(".pro-data").classList.add("hidden");
+// }    
+ 
+    function handleCredentialResponse(response) {
+       let re= decodeJwtResponse(response.credential);
+    } 
+    function decodeJwtResponse(token){
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+  
+      return JSON.parse(jsonPayload);
+  }
+  function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
 loadCanvas();
 
 blackBoard();
